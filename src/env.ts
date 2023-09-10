70bfe9e735abf01866dbc62400416cfe931d07fb
xRepZ <xzbnyby@yahoo.com>
2023-09-11 01:43:10 +0300

    docker ang nginx conf for https

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 export const basePathUrl = '/api/'
 export const mysqlOptions = {
     connectionLimit: 16,
-    host: '127.0.0.1',
+    host: 'todolistdb',
     user: 'dakz',
     password: '123321',
     database: 'todo'
